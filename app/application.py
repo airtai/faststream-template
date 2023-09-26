1ecbfcc183a1ae83ddd5ffd79f509c26cf0294f5
Kumaran Rajendhiran <kumaran@airt.ai>
2023-09-26 15:07:31 +0530

    Add faststream-gen generated files

---
+++ app/application.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+from faststream import FastStream, Logger
+from faststream.kafka import KafkaBroker
+
+class Name(BaseModel):
+    name: str = Field(..., description="Name of the person")
+
+class Greeting(BaseModel):
+    greeting: str = Field(..., description="Greeting message")
+
+broker = KafkaBroker("localhost:9092")
+app = FastStream(broker)
+
+to_greetings = broker.publisher("greetings")
+
+@broker.subscriber("names")
+async def on_names(msg: Name, logger: Logger) -> None:
+    result = f"hello {msg.name}"
+    greeting = Greeting(greeting=result)
+    await to_greetings.publish(greeting)
---
+++ tests/test_application.py
@@ -0,0 +1,18 @@
+import pytest
+
+from faststream.kafka import TestKafkaBroker
+
+from app.application import Name, Greeting, broker, on_names
+
+
+@broker.subscriber("greetings")
+async def on_greetings(msg: Greeting) -> None:
+    pass
+
+
+@pytest.mark.asyncio
+async def test_on_names():
+    async with TestKafkaBroker(broker):
+        await broker.publish(Name(name="John"), "names")
+        on_names.mock.assert_called_with(dict(Name(name="John")))
+        on_greetings.mock.assert_called_with(dict(Greeting(greeting="hello John")))
