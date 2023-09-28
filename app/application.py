a7f16becb8e8171817888c2ee62bedf990670b63
Davor Runje <davor@airt.ai>
2023-09-28 07:37:31 +0000

    polishing

--- app/application.py
+++ app/application.py
@@ -5,6 +5,10 @@ from faststream import FastStream, Logger
 from faststream.kafka import KafkaBroker
 from pydantic import BaseModel, Field
 
+version = "0.1.0"
+title = "My FastStream service"
+description = "Description of my FastStream service"
+
 
 class Name(BaseModel):
     name: str = Field(..., description="Name of the person")
@@ -15,14 +19,15 @@ class Greeting(BaseModel):
 
 
 broker = KafkaBroker("localhost:9092")
-app = FastStream(
-    broker, title="My service", version="0.1.0", description="My service description"
+app = FastStream(broker, title=title, version=version, description=description)
+
+to_greetings = broker.publisher(
+    "greetings",
+    description="Produces a message on greetings after receiving a meesage on names",
 )
 
-to_greetings = broker.publisher("greetings")
 
-
-@broker.subscriber("names")  # type: ignore
+@broker.subscriber("names", description="Consumes messages from names topic and produces messages to greetings topic")  # type: ignore
 async def on_names(msg: Name, logger: Logger) -> None:
     result = f"hello {msg.name}"
     logger.info(result)
@@ -33,19 +38,18 @@ async def on_names(msg: Name, logger: Logger) -> None:
 @app.after_startup
 async def publish_names() -> None:
     async def _publish_names() -> None:
+        names = [
+            "Ana",
+            "Mario",
+            "Pedro",
+            "João",
+            "Gustavo",
+            "Joana",
+            "Mariana",
+            "Juliana",
+        ]
         while True:
-            name = random.choice(  # nosec
-                [
-                    "Ana",
-                    "Mario",
-                    "Pedro",
-                    "João",
-                    "Gustavo",
-                    "Joana",
-                    "Mariana",
-                    "Juliana",
-                ],
-            )
+            name = random.choice(names)  # nosec
             await broker.publish(Name(name=name), topic="names")
             await asyncio.sleep(2)
 
